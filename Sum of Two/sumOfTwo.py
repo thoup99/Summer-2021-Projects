7c47e0cc71e3fffc08b5150e5c8f2b7b502cdb53
thoup99 <45801047+thoup99@users.noreply.github.com>
2021-08-17 00:36:18 -0400

    Add files via upload

--- Sum of Two/sumOfTwo.py
+++ Sum of Two/sumOfTwo.py
@@ -1,1 +1,26 @@
+def sumoftwo(itemA, itemB, targ):
+    for a in itemA:
+        for b in itemB:
+            if (a + b == targ):
+                return (True)
+    return (False)
 
+def sumoftwo1(itemA, itemB, targ):
+    for item in itemA:
+        if (targ - item in itemB):
+            return(True)
+    return(False)
+
+def sumoftwo2(itemA, itemB, targ):
+    comp = []
+    for item in itemA:
+        comp.append(targ - item)
+    for item in itemB:
+        if (item in comp):
+            return(True)
+    return(False)
+
+a = [0, 0, -5, 30212]
+b = [-10, 40, -3, 9]
+targ = -8
+print(sumoftwo2(a, b, targ))
